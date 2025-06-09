d3dbe8aa0808927e2c3b53ed4830b319958ee4a3
cjPoledo <cjpoledo26@gmail.com>
2025-06-09 15:19:58 +0800

    [ADD] issuances search and rc code correction

--- dev/upmin_stock/models/rc.py
+++ dev/upmin_stock/models/rc.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 class RC(models.Model):
     _name = "upmin_stock.rc"
     _description = "Responsibility Center"
-    _rec_name = "rc_name"
+    _rec_name = "rc_code"
 
     rc_code = fields.Char(string="RC Code", required=True)
     rc_name = fields.Char(string="RC Name", required=True)
