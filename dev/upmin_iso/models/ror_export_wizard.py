62b84ec36181b682ea038adfa5ebe13dc45f562b
cjPoledo <cjpoledo26@gmail.com>
2025-09-26 14:18:10 +0800

    [ADD] ror excel headers

--- dev/upmin_iso/models/ror_export_wizard.py
+++ dev/upmin_iso/models/ror_export_wizard.py
@@ -13,6 +13,12 @@ class RORExportWizard(models.TransientModel):
         required=True,
         domain=lambda self: self._get_office_domain(),
     )
+    related_ror = fields.One2many(
+        comodel_name="upmin_iso.ror",
+        inverse_name="office",
+        string="Related ROR",
+        readonly=True,
+    )
     export_file = fields.Binary("Export File", readonly=True)
     export_filename = fields.Char("File Name")
 
@@ -23,9 +29,16 @@ class RORExportWizard(models.TransientModel):
         return [("doc_controllers", "in", self.env.user.partner_id.id)]
 
     def action_export_ror(self):
+        # fetch review dates
+        review_dates = (
+            self.env["upmin_iso.review_period"].search(domain=[]).mapped("review_date")
+        )
+        date_dict = {}
+        for date in review_dates:
+            date_dict.setdefault(date.year, []).append(date)
+
         output = io.BytesIO()
         workbook = xlsxwriter.Workbook(output, {"in_memory": True})
-        sheet = workbook.add_worksheet("ROR")
 
         # formats
         format_default = workbook.add_format(
@@ -42,15 +55,137 @@ class RORExportWizard(models.TransientModel):
         format_sub_title = workbook.add_format(
             {"font_name": "Calibri", "font_size": 12, "bold": True}
         )
+        format_table_header = workbook.add_format(
+            {
+                "font_name": "Calibri",
+                "font_size": 11,
+                "bold": True,
+                "align": "center",
+                "valign": "vcenter",
+                "text_wrap": True,
+                "border": 1,
+            }
+        )
+        format_red_underline_text = workbook.add_format(
+            {
+                "font_name": "Calibri",
+                "font_size": 11,
+                "font_color": "red",
+                "underline": True,
+                "bold": True,
+                "align": "center",
+                "valign": "vcenter",
+                "text_wrap": True,
+                "border": 1,
+            }
+        )
 
-        # column width
-        sheet.set_column("A:A", 3.56, format_default)
-        sheet.set_column("B:B", 54.33, format_default)
-        sheet.set_column("C:E", 23.22, format_default)
-        sheet.set_column("F:H", 21.89, format_default)
+        for year in sorted(date_dict.keys(), reverse=True):
+            sheet = workbook.add_worksheet(str(year))
+            # column width
+            sheet.set_column("A:A", 3.56, format_default)
+            sheet.set_column("B:B", 54.33, format_default)
+            sheet.set_column("C:E", 23.22, format_default)
+            sheet.set_column("F:H", 21.89, format_default)
 
-        sheet.write("B1", "RISKS and OPPORTUNITIES REGISTER (ROR)", format_main_title)
-        sheet.write("B2", f"Department: {self.office.name}", format_sub_title)
+            sheet.write(
+                "B1",
+                f"RISKS and OPPORTUNITIES REGISTER (ROR) {year}",
+                format_main_title,
+            )
+            sheet.write("B2", f"Department: {self.office.name}", format_sub_title)
+
+            # headers
+            sheet.merge_range("A4:A6", "#", format_table_header)
+            sheet.merge_range("B4:B6", "Requirement/Issue", format_table_header)
+            sheet.merge_range("C4:C6", "Interested Parties", format_table_header)
+            sheet.merge_range("D4:D6", "Needs and Expectations", format_table_header)
+            sheet.merge_range("E4:E6", "Compliance\nObligations", format_table_header)
+            sheet.merge_range(
+                "F4:F6", "Risks (R) /\nOpportunities (O) ", format_table_header
+            )
+            sheet.merge_range(
+                "G4:G6", "Consequence (C) /\nBenefit (B)", format_table_header
+            )
+            sheet.merge_range("H4:H6", "Existing Control", format_table_header)
+
+            start_col = 8
+            for date in date_dict[year]:
+                # set column size
+                sheet.set_column(start_col, start_col + 2, 10.33, format_default)
+                sheet.set_column(start_col + 3, start_col + 3, 4.11, format_default)
+                sheet.set_column(start_col + 4, start_col + 4, 7.11, format_default)
+                sheet.set_column(start_col + 5, start_col + 5, 16.89, format_default)
+                sheet.set_column(start_col + 6, start_col + 6, 16.78, format_default)
+                sheet.set_column(start_col + 7, start_col + 7, 18.89, format_default)
+                sheet.set_column(start_col + 8, start_col + 8, 19.11, format_default)
+                sheet.set_column(start_col + 9, start_col + 9, 27.89, format_default)
+
+                # put headers
+                sheet.merge_range(
+                    3,
+                    start_col,
+                    3,
+                    start_col + 4,
+                    "Inherent/Residual Risks",
+                    format_table_header,
+                )
+                sheet.merge_range(
+                    4, start_col, 4, start_col + 1, "O", format_table_header
+                )
+                sheet.write(5, start_col, "L", format_table_header)
+                sheet.write(5, start_col + 1, "F", format_table_header)
+                sheet.merge_range(
+                    4, start_col + 2, 5, start_col + 2, "S", format_table_header
+                )
+                sheet.merge_range(
+                    4, start_col + 3, 5, start_col + 4, "RR/OR", format_table_header
+                )
+                sheet.merge_range(
+                    3,
+                    start_col + 5,
+                    5,
+                    start_col + 6,
+                    "Conclusion\n(Significant /  Not Significant)",
+                    format_table_header,
+                )
+                sheet.merge_range(
+                    3,
+                    start_col + 7,
+                    5,
+                    start_col + 7,
+                    "Required Action",
+                    format_table_header,
+                )
+                sheet.merge_range(
+                    3,
+                    start_col + 8,
+                    5,
+                    start_col + 8,
+                    "Responsible/Date",
+                    format_table_header,
+                )
+                date_str = date.strftime("%B %d, %Y")
+                sheet.write(
+                    3, start_col + 9, f"Review Date:\n{date_str}", format_table_header
+                )
+                sheet.write_rich_string(
+                    3,
+                    start_col + 9,
+                    "Review Date:\n",
+                    format_red_underline_text,
+                    date_str,
+                    format_table_header,
+                )
+                sheet.merge_range(
+                    4,
+                    start_col + 9,
+                    5,
+                    start_col + 9,
+                    "Status / Results",
+                    format_table_header,
+                )
+                start_col += 10
 
         workbook.close()
         data_bytes = output.getvalue()
