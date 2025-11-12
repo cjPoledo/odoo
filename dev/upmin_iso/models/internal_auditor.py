46ae8df4e37100dd37ab35fbcdbe8275be587d19
cjPoledo <cjpoledo26@gmail.com>
2025-11-12 11:05:08 +0800

    [ADD] add users to internal auditor group automatically

--- dev/upmin_iso/models/internal_auditor.py
+++ dev/upmin_iso/models/internal_auditor.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class InternalAuditor(models.Model):
@@ -20,6 +20,11 @@ class InternalAuditor(models.Model):
     )
     trained = fields.Boolean(string="Trained", default=False)
     certified = fields.Boolean(string="Certified", default=False)
+    have_internal_auditor_perms = fields.Boolean(
+        string="Have Internal Auditor Permissions?",
+        readonly=True,
+        compute="_compute_have_internal_auditor_perms",
+    )
 
     _sql_constraints = [
         (
@@ -28,3 +33,39 @@ class InternalAuditor(models.Model):
             "Auditor already exists.",
         ),
     ]
+
+    def _compute_have_internal_auditor_perms(self):
+        group = self.env.ref("upmin_iso.group_iso_internal_auditor")
+
+        for rec in self:
+            user = rec.name.user_id
+            rec.have_internal_auditor_perms = user and group in user.groups_id
+
+    def _assign_internal_auditor_group(self):
+        group = self.env.ref("upmin_iso.group_iso_internal_auditor")
+
+        for rec in self:
+            user = rec.name.user_id
+            if user:
+                user.sudo().write({"groups_id": [(4, group.id)]})
+
+    @api.model
+    def create(self, vals):
+        record = super().create(vals)
+        record._assign_internal_auditor_group()
+        return record
+
+    def write(self, vals):
+        record = super().write(vals)
+        self._assign_internal_auditor_group()
+        return record
+
+    def unlink(self):
+        group = self.env.ref("upmin_iso.group_iso_internal_auditor")
+
+        for rec in self:
+            user = rec.name.user_id
+            if user and group in user.groups_id:
+                user.sudo().write({"groups_id": [(3, group.id)]})
+
+        return super().unlink()
