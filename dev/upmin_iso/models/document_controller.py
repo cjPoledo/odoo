4d39efc870dc58409be4849247570c899cd937ef
cjPoledo <cjpoledo26@gmail.com>
2025-11-12 10:52:26 +0800

    [ADD] add users to doc control group automatically

--- dev/upmin_iso/models/document_controller.py
+++ dev/upmin_iso/models/document_controller.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class DocumentController(models.Model):
@@ -19,6 +19,11 @@ class DocumentController(models.Model):
         store=True,
     )
     trained = fields.Boolean(string="Trained", default=False)
+    have_doc_control_perms = fields.Boolean(
+        string="Have Document Control Permissions?",
+        readonly=True,
+        compute="_compute_have_doc_control_perms",
+    )
 
     _sql_constraints = [
         (
@@ -27,3 +32,39 @@ class DocumentController(models.Model):
             "Document Controller already exists.",
         ),
     ]
+
+    def _compute_have_doc_control_perms(self):
+        group = self.env.ref("upmin_iso.group_iso_doc_controller")
+
+        for rec in self:
+            user = rec.name.user_id
+            rec.have_doc_control_perms = user and group in user.groups_id
+
+    def _assign_doc_controller_group(self):
+        group = self.env.ref("upmin_iso.group_iso_doc_controller")
+
+        for rec in self:
+            user = rec.name.user_id
+            if user:
+                user.sudo().write({"groups_id": [(4, group.id)]})
+
+    @api.model
+    def create(self, vals):
+        record = super().create(vals)
+        record._assign_doc_controller_group()
+        return record
+
+    def write(self, vals):
+        record = super().write(vals)
+        self._assign_doc_controller_group()
+        return record
+
+    def unlink(self):
+        group = self.env.ref("upmin_iso.group_iso_doc_controller")
+
+        for rec in self:
+            user = rec.name.user_id
+            if user and group in user.groups_id:
+                user.sudo().write({"groups_id": [(3, group.id)]})
+
+        return super().unlink()
