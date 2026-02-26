52b68b4531c49415f9c0b5e45fd938741c54c25d
cjPoledo <cjpoledo26@gmail.com>
2026-02-26 15:10:29 +0800

    [FIX] security rules error

--- dev/upmin_iso/models/ccar_correction_effectiveness.py
+++ dev/upmin_iso/models/ccar_correction_effectiveness.py
@@ -15,6 +15,24 @@ class CCARCorrectionEffectiveness(models.Model):
     )
     correction = fields.Text(string="Correction")
     verification = fields.Text(string="How was verification performed?")
-    verified_by = fields.Many2one(comodel_name="hr.employee", string="Who verified?")
+    allowed_verifiers = fields.Many2many(
+        comodel_name="hr.employee",
+        compute="_compute_allowed_verifiers",
+        store=False,
+    )
+    verified_by = fields.Many2one(
+        comodel_name="hr.employee",
+        string="Who verified?",
+        domain="[('id', 'in', allowed_verifiers)]",
+    )
     verified_date = fields.Date(string="Verified Date")
     approval = fields.Text(string="Approved/Rejected")
+
+    def _compute_allowed_verifiers(self):
+        for rec in self:
+            if rec.ccar.related_nc and rec.ccar.related_nc.audit_info:
+                rec.allowed_verifiers = (
+                    rec.ccar.related_nc.audit_info.internal_auditors.mapped("name")
+                )
+            else:
+                rec.allowed_verifiers = False
--- dev/upmin_iso/models/ccar_corrective_action_effectiveness.py
+++ dev/upmin_iso/models/ccar_corrective_action_effectiveness.py
@@ -15,6 +15,25 @@ class CCARCorrectiveActionEffectiveness(models.Model):
     )
     corrective_action = fields.Text(string="Corrective Action")
     verification = fields.Text(string="How was verification performed?")
-    verified_by = fields.Many2one(comodel_name="hr.employee", string="Who verified?")
+    verification = fields.Text(string="How was verification performed?")
+    allowed_verifiers = fields.Many2many(
+        comodel_name="hr.employee",
+        compute="_compute_allowed_verifiers",
+        store=False,
+    )
+    verified_by = fields.Many2one(
+        comodel_name="hr.employee",
+        string="Who verified?",
+        domain="[('id', 'in', allowed_verifiers)]",
+    )
     verified_date = fields.Date(string="Verified Date")
     approval = fields.Text(string="Approved/Rejected")
+
+    def _compute_allowed_verifiers(self):
+        for rec in self:
+            if rec.ccar.related_nc and rec.ccar.related_nc.audit_info:
+                rec.allowed_verifiers = (
+                    rec.ccar.related_nc.audit_info.internal_auditors.mapped("name")
+                )
+            else:
+                rec.allowed_verifiers = False
