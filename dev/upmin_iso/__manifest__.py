c692e4d02fd7354b325912d00e3873d7c5f5fcd6
cjPoledo <cjpoledo26@gmail.com>
2026-08-12 23:41:33 +0800

    chore(upmin_iso): bump version to 1.5

--- dev/upmin_iso/__manifest__.py
+++ dev/upmin_iso/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "ISO",
     "author": "Clent Japhet Poledo",
-    "version": "1.4",
+    "version": "1.5",
     "summary": "Tools for managing ISO-related documents",
     "application": True,
     "depends": [
